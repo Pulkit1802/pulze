c473ba4d678bc96216d6997bc1e135de637c00c4
SYSHIL <ilhan.syed@gmail.com>
2022-09-04 11:37:58 +0530

    add temperature sense

---
+++ SenseTemperature/passData.py
@@ -0,0 +1,20 @@
+from http import client
+import time 
+import serial 
+import pymongo
+from pymongo import MongoClient
+arduinoData = serial.Serial('com5',115200)
+time.sleep(1)
+cluster = MongoClient("mongodb+srv://ds-hack:XrjoFN3Clc7e4i34@dshack.gk1jil8.mongodb.net/dsHack",connect=False)
+db = cluster["dsHack"]
+collection = db["patiendatas"]
+
+while True:
+    while(arduinoData.inWaiting()==0):
+        pass 
+    #Ready to read 
+    dataPacket = arduinoData.readline()
+    dataPacket = str(dataPacket,'utf-8')
+    print(dataPacket)
+    time.sleep(5)
+    collection.find_one_and_update({'name':'sara'},{'$set':{'age':dataPacket}})
